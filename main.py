b8ed74096e14300e96489c02008059ab510aa878
EliasH-glith <188242446+EliasH-glitch@users.noreply.github.com>
2026-05-13 21:40:07 +0200

    Full list of meal + FINALLY can order meals

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import time as tm
 import sys
 from art import text2art
-from strava_cz import StravaCZ, AuthenticationError, MealType
+from strava_cz import StravaCZ, AuthenticationError, MealType, InvalidMealTypeError, InsufficientBalanceError
 import os
 from dotenv import load_dotenv
 
@@ -17,6 +17,7 @@ global strava_log
 strava_log = "any data"
 
 def text_animation(text):
+
     for i in text:
         sys.stdout.write(i)
         sys.stdout.flush()
@@ -24,10 +25,12 @@ def text_animation(text):
     print()
     
 def intro_text_ascii():    
+
     Cool_title = text2art("Strava.sh")
     print(Cool_title)
 
 def strava_response_request():
+
     global strava_log
     try:
         strava_log = StravaCZ(
@@ -42,6 +45,7 @@ def strava_response_request():
     
     
 def Logout():
+    
       strava_log.logout()
       text_animation("Your are loged out now 🤓")
 
@@ -63,14 +67,37 @@ def ordered_meal():
         print(f"Date:{date}:\n Soup: {soup}\n Meal name: {name}")
         position += 1
 
+def jidelnicek_vypsani():
+
+    strava_log.menu.fetch()
+    strava_log.menu.print()
+
+def order_food():
+    text_animation("This is full menu:")
+    jidelnicek_vypsani()
+    id_to_order = int(input("Which meal by ID (the fist number) u want to order:\n"))
+
+    if strava_log.menu.is_ordered(id_to_order):
+        text_animation("Meal is already ordered")
+    
+    else:
+        try:
+            strava_log.menu.order_meals(id_to_order)
+            text_animation("Meal is now ordered")
+        except InvalidMealTypeError as error:
+            print(f"Error: {error}")
+        except InsufficientBalanceError as error:
+            print(f"Error: {error}")
+
 
 strava_response_request()
+text_animation(intro_text)
 
 while True:
-    text_animation(intro_text)
+    
     intro_text_ascii()
 
-    action = int(input(f"What u want to know??\n1. Account info\n2. Ordered food\n3. Logout\n"))
+    action = int(input(f"What u want to know??\n1. Account info\n2. Ordered food only\n3. All the food items\n4. Order food\n5. Logout\n"))
 
     if action == 1:
         print(strava_log.user)
@@ -79,6 +106,12 @@ while True:
         ordered_meal()
     
     elif action == 3:
+        jidelnicek_vypsani()
+
+    elif action == 4:
+        order_food()
+
+    elif action == 5:
         Logout()
         break
 
